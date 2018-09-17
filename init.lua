a342fb3bfda39d0fc8fbb08ab290994df55dd5fd
Jonathan <jonathan@tappister.com>
2018-09-16 18:13:26 -0700

    only configure wifi or start polling for messages if we have the right config settings in secrets.lua

--- init.lua
+++ init.lua
@@ -6,9 +6,14 @@ dofile("messages.lua")
 local wifi_conf = {}
 wifi_conf.ssid = WIFI_SSID
 wifi_conf.pwd =  WIFI_PASSWORD
-wifi.setmode(wifi.STATION)
-wifi.sta.config(wifi_conf)
+if string.len(wifi_conf.ssid) > 0 then -- connect to wifi, if configured
+    wifi.setmode(wifi.STATION)
+    wifi.sta.config(wifi_conf)
+end
 
 setupController()      -- init SPI
 animateLightsOpt()     -- animate the lights to show we're alive
-checkForMessagesSoon() -- start looking for messages from the server
+
+if string.len(MESSAGE_SERVER_URL) > 0 then
+    checkForMessagesSoon() -- start looking for messages from the server, if configured
+end
